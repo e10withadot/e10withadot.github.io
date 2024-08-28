9a4c5383c0733c3b960f612d407cfeeb6a9ebf82
e10withadot <91983480+e10withadot@users.noreply.github.com>
2024-08-29 00:22:53 +0300

    attempting to fix iOS scroll lock
    i added more properties to the "disable-scroll" class

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,10 +1,23 @@
 import { em2px } from '/assets/js/em2px.js';
 
+const $body = document.querySelector('body');
+let scrollPosition = 0;
+
 function dropOrDont() {
     var arrow = document.getElementById("arrow");
+    const topbtn = document.getElementById("topbtn");
+
     document.getElementById("down").classList.toggle("show");
-    document.getElementById("topbtn").classList.toggle("visible");
+    if (topbtn != null)
+        topbtn.classList.toggle("visible");
     document.body.classList.toggle("disable-scroll");
+    scrollPosition = window.scrollY;
+    if (document.body.classList.contains("disable-scroll"))
+        document.body.style.top = `-${scrollPosition}px`;
+    else {
+        document.body.style.removeProperty('top');
+        window.scrollTo(0, scrollPosition);
+    }
     arrow.innerHTML = arrow.innerHTML === "V" ? "X" : "V";
 }
 
