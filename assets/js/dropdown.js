1e3dcb83b67dfd26c455077bb4a32d665dc4a90d
e10withadot <91983480+e10withadot@users.noreply.github.com>
2024-08-31 21:32:13 +0300

    put the nav and dropdown content in one wrapper
    hopefully THIS will fix iOS scroll lock lol

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,33 +1,27 @@
 import { em2px } from '/assets/js/em2px.js';
-import { clearAllBodyScrollLocks, disableBodyScroll } from '/assets/js/bodyScrollLock.es6.js';
+import { enableBodyScroll, disableBodyScroll } from '/assets/js/bodyScrollLock.es6.js';
 
-var arrow = document.getElementById("arrow");
 const down = document.getElementById("down");
-const wrapper = document.getElementById("down-wrapper");
-const nav = document.getElementById("nav");
+const wrapper = document.getElementById("nav-wrapper");
 
 function dropOrDont() {
     down.classList.toggle("show");
     if (down.classList.contains("show")){
-        disableBodyScroll(nav);
         disableBodyScroll(wrapper);
     }
-    else clearAllBodyScrollLocks();
+    else enableBodyScroll(wrapper);
     arrow.innerHTML = arrow.innerHTML === "V" ? "X" : "V";
 }
 
 window.dropOrDont = dropOrDont;
 
 window.addEventListener('resize', function(){
-    if (down.classList.contains("show")){
-        if (window.innerWidth > em2px(42)) {
-            down.classList.remove("show");
-            clearAllBodyScrollLocks();
-        }
-        else {
-            down.classList.add("show");
-            disableBodyScroll(nav);
-            disableBodyScroll(wrapper);
-        }
+    if (window.innerWidth > em2px(42) && down.classList.contains("show")) {
+        down.classList.remove("show");
+        enableBodyScroll(wrapper);
+    }
+    else if (window.innerWidth < em2px(42) && !(down.classList.contains("show"))){
+        down.classList.add("show");
+        disableBodyScroll(wrapper);
     }
 });
